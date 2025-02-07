82f2f17b9d37c519de3bdfcf0819256610617f81
Sergio Sanz <sergiosanz2004@gmail.com>
2025-02-07 23:37:42 +0100

    fix: collapse navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { CustomCursorComponent } from '../custom-cursor/cursor-component';
 
 @Component({
@@ -9,8 +9,21 @@ import { CustomCursorComponent } from '../custom-cursor/cursor-component';
 
 export class NavbarComponent {
 
+  @ViewChild('navbarNavAltMarkup') navbarNav!: ElementRef;
+
+
   constructor(private customCursor: CustomCursorComponent) {}
 
+
+
+  closeNav() {
+    if (this.navbarNav) {
+      const navbarCollapse = this.navbarNav.nativeElement;
+      if (navbarCollapse.classList.contains('show')) {
+        navbarCollapse.classList.remove('show'); // Cierra el menú
+      }
+    }
+  }
 
   onMouseEnter() {
     this.customCursor.setIsHovered(true);
